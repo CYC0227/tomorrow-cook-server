2141d727119bccfebc744a257a3f411702a96e9d
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-05-22 15:40:15 +0900

    yc

--- src/main/java/tomorrowcook/mp/controller/PostingController.java
+++ src/main/java/tomorrowcook/mp/controller/PostingController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import tomorrowcook.mp.domain.Member;
 import tomorrowcook.mp.domain.Posting;
 import tomorrowcook.mp.service.PostingService;
@@ -21,6 +18,28 @@ public class PostingController {
     private final PostingService postingService;
 
     //추가
+
+    @GetMapping(value = "/postings")
+    public List<PostingForm> postingForms() {
+        List<Posting> postings = postingService.findPostings();
+        List<PostingForm> postingForms;
+        PostingForm postingForm;
+
+        for (Posting p : postings) {
+            String imgURL1 = p.getImgURL();
+            String title1 = p.getTitle();
+            String description1 = p.getDescription();
+            String information1 = p.getInformation();
+            String ingredients_name1 = p.getIngredients_name();
+            String ingredients_quantity1 = p.getIngredients_quantity();
+
+
+            postingForm = new PostingForm(imgURL1, title1, description1, information1, ingredients_name1, ingredients_quantity1);
+
+            postingForms.add(postingForm);
+        }
+        return postingForms;
+    }
 
 
     @PostMapping(value = "/postings/new")
--- src/main/java/tomorrowcook/mp/controller/PostingForm.java
+++ src/main/java/tomorrowcook/mp/controller/PostingForm.java
@@ -1,10 +1,21 @@
 package tomorrowcook.mp.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 public class PostingForm {
+
+    public PostingForm(String imgURL, String title, String description, String information, String ingredients_name, String ingredients_quantity){
+        this.imgURL = imgURL;
+        this.title = title;
+        this.description = description;
+        this.information = information;
+        this.ingredients_name = ingredients_name;
+        this.ingredients_quantity = ingredients_quantity;
+    }
 
     private String imgURL;
     private String title;
