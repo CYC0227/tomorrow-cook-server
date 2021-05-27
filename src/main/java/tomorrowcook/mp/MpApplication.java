cfb55101ea61e4f6e3763c0abbc645dbe64f1ff3
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-05-28 01:51:13 +0900

    파일 api 가능

--- src/main/java/tomorrowcook/mp/MpApplication.java
+++ src/main/java/tomorrowcook/mp/MpApplication.java
@@ -2,8 +2,13 @@ package tomorrowcook.mp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tomorrowcook.mp.file.FileUploadProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileUploadProperties.class
+})
 public class MpApplication {
 
 	public static void main(String[] args) {
